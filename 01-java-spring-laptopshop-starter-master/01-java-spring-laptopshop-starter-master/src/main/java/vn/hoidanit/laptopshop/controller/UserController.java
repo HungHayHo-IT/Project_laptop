3c49104cdb0b614a621a81ad076c0588d9dbe24f
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2025-04-24 08:19:24 +0700

    reponsitory

--- 01-java-spring-laptopshop-starter-master/01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter-master/01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,10 +1,14 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
@@ -12,10 +16,11 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
@@ -28,23 +33,14 @@ public class UserController {
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         String test = this.userService.handHello();
-        model.addAttribute("eric", test);
-        model.addAttribute("nguyenhung", "from controller with model");
+        model.addAttribute("newUser", new User());
         return "/admin/user/create";
     }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User nguyenhung) {
+        System.out.println("run here" + nguyenhung);
+        this.userService.handleSaveUser(nguyenhung);
+        return "hello";
+    }
 }
-
-// @RestController
-// public class UserController {
-
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handHello();
-// }
-// }
