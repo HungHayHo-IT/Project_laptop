37fcc76c25d2e96ff310cdda3935dedd84a2800b
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2025-04-29 11:31:07 +0700

    one-to-many

--- 01-java-spring-laptopshop-starter-master/01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ 01-java-spring-laptopshop-starter-master/01-java-spring-laptopshop-starter-master/src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,12 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 
 public class User {
     @Id
@@ -19,6 +25,14 @@ public class User {
     private String phone;
 
     private String avatar;
+
+    // User many -> to -> one role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     public User() {
     }
